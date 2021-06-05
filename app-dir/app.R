9f1439156ed8ffa738557509955a00b99a7c7fb0
kellobri <kellyobriant@gmail.com>
2021-06-04 19:14:31 -0700

    test update in place

--- app-dir/app.R
+++ app-dir/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data - Azure DevOps Demo"),
+    titlePanel("Old Faithful - Azure DevOps Demo"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
